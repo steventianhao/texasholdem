ea00789d48ff0781d1128be3d5cb8736448ca8db
simon.sammi <simon.sammi@gmail.com>
2014-07-24 14:47:50 +0800

    caculate the result and add tests

--- apps/game_server/src/poker_rules.erl
+++ apps/game_server/src/poker_rules.erl
@@ -1,5 +1,5 @@
 -module(poker_rules).
--export([combinations/2,is_flush/1,is_straight/1,poker_combos/1,all_cards/0]).
+-export([all_cards/0,result/1,card/1]).
 
 -define(HEART,$H).
 -define(DIAMOND,$D).
@@ -51,6 +51,11 @@ value(#card{rank=?QUEEN})->
 value(#card{rank=?KING})->
 	13.
 
+card([S,R])->
+	[#card{rank=R,suit=S}];
+card([S,R|T])->
+	[#card{rank=R,suit=S}|card(T)].
+
 poker_combos(Cards) when length(Cards)==7 ->
 	combinations(Cards,5).
 
@@ -63,6 +68,20 @@ combinations(L,1)->
 combinations([H|T],N)->
 	lists:foldl(fun(L,AccIn)->[[H|L]|AccIn] end,combinations(T,N),combinations(T,N-1)).
 
+group_by(List,KeyFunc) ->
+	Fun = fun(C,Acc)->
+			V=KeyFunc(C),
+			case lists:keyfind(V,1,Acc) of
+				false->
+					[{V,[C],1}|Acc];
+				{V,L,N}->
+					[{V,[C|L],N+1}|lists:keydelete(V,1,Acc)]
+			end
+	end,
+	lists:foldl(Fun,[],List).
+
+
+
 is_flush([H|_]=Cards) when length(Cards)==5 ->
 	lists:all(fun(C)->C#card.suit==H#card.suit end, Cards).
 
@@ -71,5 +90,55 @@ is_straight(Cards) when length(Cards)==5 ->
 	[A,B,C,D,E]=lists:sort(fun(C1,C2)->C1 =< C2 end,Vs),
 	B-A==1 andalso C-B==1 andalso D-C==1 andalso E-D==1.
 
+sort_cards_desc(Cards)->
+	lists:sort(fun(C1,C2)->value(C1) >= value(C2) end,Cards).
+
 all_cards()->
 	[#card{suit=S,rank=R}||S<-?SUITS,R<-?RANKS].
+
+
+
+-record(poker,{pattern=undefined,cards=undefined}).
+
+pattern(Name,Cards)->
+	#poker{pattern=Name,cards=Cards}.
+
+straight_flush([#card{rank=?ACE}|_]=Cards)->
+	#poker{pattern=royal_flush,cards=Cards};
+straight_flush(Cards)->
+	#poker{pattern=straight_flush,cards=Cards}.
+
+sort_func({V1,_Cs1,N1},{V2,_Cs2,N2}) when N1==N2->
+	V1 >= V2;
+sort_func({_V1,_Cs1,N1},{_V2,_Cs2,N2})->
+	N1 > N2.
+
+other_result(Cards)->
+	R=lists:sort(fun sort_func/2,group_by(Cards,fun(C)->value(C) end)),
+	case R of
+		[{_,_,4},{_,_,1}]->
+			pattern(four_of_a_kind,Cards);
+		[{_,_,3},{_,_,2}]->
+			pattern(full_house,Cards);
+		[{_,_,3},{_,_,1},{_,_,1}]->
+			pattern(three_of_a_kind,Cards);
+		[{_,_,2},{_,_,2},{_,_,1}]->
+			pattern(two_pair,Cards);
+		[{_,_,2},{_,_,1},{_,_,1},{_,_,1}]->
+			pattern(one_pair,Cards);
+		[{_,_,1},{_,_,1},{_,_,1},{_,_,1},{_,_,1}]->
+			pattern(high_card,Cards)
+	end.
+
+result(Cards) when length(Cards)==5 ->
+	case {is_flush(Cards),is_straight(Cards)} of
+		{true,true}->
+			straight_flush(sort_cards_desc(Cards));
+		{true,false}->
+			pattern(flush,Cards);
+		{false,true}->
+			pattern(straight,Cards);
+		{false,false}->
+			other_result(Cards)
+	end.
+
--- apps/game_server/test/poker_rules_tests.erl
+++ apps/game_server/test/poker_rules_tests.erl
@@ -1,1 +1,75 @@
-poker_rules_tests
+-module(poker_rules_tests).
+-define(DEBUG,true).
+-include_lib("eunit/include/eunit.hrl").
+c(L)->
+	poker_rules:card(L).
+
+c_test()->
+	Cs=c("HT"),
+	?assertEqual([{card,$H,$T}],Cs).
+
+c2_test()->
+	Cs=c("HTS5"),
+	?assertEqual([{card,$H,$T},{card,$S,$5}],Cs).
+
+result_test()->
+	Cs=c("HTHJHQHKHA"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,royal_flush,_},Result).
+
+
+result2_test()->
+	Cs=c("HTHJHQHKH9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,straight_flush,_},Result).
+
+result3_test()->
+	Cs=c("HTSJHQHKH9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,straight,_},Result).
+
+result4_test()->
+	Cs=c("HTH3HQHKH9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,flush,_},Result).
+
+
+result5_test()->
+	Cs=c("HTSTDTCTH9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,four_of_a_kind,_},Result).
+
+result6_test()->
+	Cs=c("HTSTDTC9H9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,full_house,_},Result).
+
+result7_test()->
+	Cs=c("HTS3DTC9H9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,two_pair,_},Result).
+
+result8_test()->
+	Cs=c("HTS3D9C9H9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,three_of_a_kind,_},Result).
+
+result9_test()->
+	Cs=c("HTS3D7C9H9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,one_pair,_},Result).
+
+result10_test()->
+	Cs=c("HTS3D7C8H9"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({poker,high_card,_},Result).
