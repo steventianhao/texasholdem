bc08d2f297cd73a05f531ad1a12c9a187fe20f0f
simon.sammi <simon.sammi@gmail.com>
2014-07-24 18:39:32 +0800

    add tests, and compare cards

--- apps/game_server/src/poker_rules.erl
+++ apps/game_server/src/poker_rules.erl
@@ -1,15 +1,6 @@
 -module(poker_rules).
--export([result/1]).
+-export([result/1,compare/2]).
 -include("poker_cards.hrl").
--record(poker,{pattern=undefined,cards=undefined}).
-
-pattern(Name,Cards)->
-	#poker{pattern=Name,cards=Cards}.
-
-straight_flush([#card{rank=?ACE}|_]=Cards)->
-	#poker{pattern=royal_flush,cards=Cards};
-straight_flush(Cards)->
-	#poker{pattern=straight_flush,cards=Cards}.
 
 sort_func({V1,_Cs1,N1},{V2,_Cs2,N2}) when N1==N2->
 	V1 >= V2;
@@ -30,18 +21,18 @@ group_by(Cards) ->
 other_result(Cards)->
 	R=lists:sort(fun sort_func/2,group_by(Cards)),
 	case R of
-		[{_,_,4},{_,_,1}]->
-			pattern(four_of_a_kind,Cards);
-		[{_,_,3},{_,_,2}]->
-			pattern(full_house,Cards);
-		[{_,_,3},{_,_,1},{_,_,1}]->
-			pattern(three_of_a_kind,Cards);
-		[{_,_,2},{_,_,2},{_,_,1}]->
-			pattern(two_pair,Cards);
-		[{_,_,2},{_,_,1},{_,_,1},{_,_,1}]->
-			pattern(one_pair,Cards);
-		[{_,_,1},{_,_,1},{_,_,1},{_,_,1},{_,_,1}]->
-			pattern(high_card,Cards)
+		[{K2,_,4},{K1,_,1}]->
+			{four_of_a_kind,?FOUR_OF_A_KIND+K2*10+K1};
+		[{K2,_,3},{K1,_,2}]->
+			{full_house,?FULL_HOUSE+K2*10+K1};
+		[{K3,_,3},{K2,_,1},{K1,_,1}]->
+			{three_of_a_kind,?THREE_OF_A_KIND+K3*100+K2*10+K1};
+		[{K3,_,2},{K2,_,2},{K1,_,1}]->
+			{two_pair,?TWO_PAIR+K3*100+K2*10+K1};
+		[{K4,_,2},{K3,_,1},{K2,_,1},{K1,_,1}]->
+			{one_pair,?ONE_PAIR+K4*1000+K3*100+K2*10+K1};
+		[{K5,_,1},{K4,_,1},{K3,_,1},{K2,_,1},{K1,_,1}]->
+			{high_card,?HIGH_CARD+K5*10000+K4*1000+K3*100+K2*10+K1}
 	end.
 
 is_flush([H|_]=Cards)->
@@ -49,7 +40,16 @@ is_flush([H|_]=Cards)->
 
 is_straight(Cards)->
 	[A,B,C,D,E]=[poker_cards:value(C)||C<-Cards],
-	A-B==1 andalso B-C==1 andalso C-D==1 andalso D-E==1.
+	case B-C==1 andalso C-D==1 andalso D-E==1 of
+		true-> 
+			case A-B of
+				1->{true,A};
+				9->{true,B};
+				_->false
+			end;
+		false->
+			false
+	end.
 
 sort_cards_desc(Cards)->
 	lists:sort(fun(C1,C2)->poker_cards:value(C1) >= poker_cards:value(C2) end,Cards).
@@ -57,12 +57,22 @@ sort_cards_desc(Cards)->
 result(Cards) when length(Cards)==5 ->
 	Scs=sort_cards_desc(Cards),
 	case {is_flush(Scs),is_straight(Scs)} of
-		{true,true}->
-			straight_flush(Scs);
+		{true,{true,14}}->
+			{royal_flush,?ROYAL_FLUSH};
+		{true,{true,Max}}->
+			{straight_flush,?STRAIGHT_FLUSH+Max};
 		{true,false}->
-			pattern(flush,Scs);
-		{false,true}->
-			pattern(straight,Scs);
+			[A,B,C,D,E]=[poker_cards:value(C)||C<-Scs],
+			{flush,?FLUSH+A*10000+B*1000+C*100+D*10+E};
+		{false,{true,Max}}->
+			{straight,?STRAIGHT+Max};
 		{false,false}->
 			other_result(Scs)
 	end.
+
+comp(A,B) when is_integer(A) andalso is_integer(B) andalso A==B-> 0;
+comp(A,B) when is_integer(A) andalso is_integer(B) andalso A >B-> 1;
+comp(A,B) when is_integer(A) andalso is_integer(B) -> -1.
+
+compare({_P1,V1},{_P2,V2})->
+	comp(V1,V2).
--- apps/game_server/test/poker_rules_tests.erl
+++ apps/game_server/test/poker_rules_tests.erl
@@ -1,6 +1,8 @@
 -module(poker_rules_tests).
 -define(DEBUG,true).
 -include_lib("eunit/include/eunit.hrl").
+-include("poker_cards.hrl").
+
 c(L)->
 	poker_cards:card(L).
 
@@ -13,63 +15,86 @@ c2_test()->
 	?assertEqual([{card,$H,$T},{card,$S,$5}],Cs).
 
 result_test()->
-	Cs=c("HTHJHQHKHA"),
-	Result=poker_rules:result(Cs),
+	Result=poker_rules:result(c("HTHJHQHKHA")),
 	?debugVal(Result),
-	?assertMatch({poker,royal_flush,_},Result).
+	?assertMatch({royal_flush,?ROYAL_FLUSH},Result).
 
+compare_test()->
+	R1=poker_rules:result(c("HTHJHQHKHA")),
+	R2=poker_rules:result(c("CTCJCQCKCA")),
+	Comp1=poker_rules:compare(R1,R2),
+	?assertEqual(0,Comp1),
+	Comp2=poker_rules:compare(R2,R1),
+	?assertEqual(0,Comp2).
 
 result2_test()->
-	Cs=c("HTHJHQHKH9"),
+	Result=poker_rules:result(c("HTHJHQHKH9")),
+	?debugVal(Result),
+	?assertMatch({straight_flush,?STRAIGHT_FLUSH+13},Result).
+
+result2a_test()->
+	Cs=c("H5H4H3H2HA"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,straight_flush,_},Result).
+	?assertMatch({straight_flush,?STRAIGHT_FLUSH+5},Result).
 
 result3_test()->
 	Cs=c("HTSJHQHKH9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,straight,_},Result).
+	?assertMatch({straight,?STRAIGHT+13},Result).
+
+result3a_test()->
+	Cs=c("H5S4H3H2HA"),
+	Result=poker_rules:result(Cs),
+	?debugVal(Result),
+	?assertMatch({straight,?STRAIGHT+5},Result).
+
+compare3_test()->
+	R1=poker_rules:result(c("HTSJHQHKH9")),
+	R2=poker_rules:result(c("H5S4H3H2HA")),
+	Comp=poker_rules:compare(R1,R2),
+	?assertEqual(1,Comp).
 
 result4_test()->
 	Cs=c("HTH3HQHKH9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,flush,_},Result).
+	?assertMatch({flush,?FLUSH+13*10000+12*1000+10*100+9*10+3},Result).
 
 
 result5_test()->
 	Cs=c("HTSTDTCTH9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,four_of_a_kind,_},Result).
+	?assertMatch({four_of_a_kind,?FOUR_OF_A_KIND+10*10+9},Result).
 
 result6_test()->
 	Cs=c("HTSTDTC9H9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,full_house,_},Result).
+	?assertMatch({full_house,?FULL_HOUSE+10*10+9},Result).
 
 result7_test()->
 	Cs=c("HTS3DTC9H9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,two_pair,_},Result).
+	?assertMatch({two_pair,?TWO_PAIR+10*100+9*10+3},Result).
 
 result8_test()->
 	Cs=c("HTS3D9C9H9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,three_of_a_kind,_},Result).
+	?assertMatch({three_of_a_kind,?THREE_OF_A_KIND+9*100+10*10+3},Result).
 
 result9_test()->
 	Cs=c("HTS3D7C9H9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,one_pair,_},Result).
+	?assertMatch({one_pair,?ONE_PAIR+9*1000+10*100+7*10+3},Result).
 
 result10_test()->
 	Cs=c("HTS3D7C8H9"),
 	Result=poker_rules:result(Cs),
 	?debugVal(Result),
-	?assertMatch({poker,high_card,_},Result).
+	?assertMatch({high_card,?HIGH_CARD+10*10000+9*1000+8*100+7*10+3},Result).
